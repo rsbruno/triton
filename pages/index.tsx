e208d30f24948f2583bc65a2b27034c244e51fa4
rsbruno <rsbruno.cdc@gmail.com>
2022-02-28 15:44:21 -0300

    feat: button background

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { RiHome2Line } from "react-icons/ri";
+import { MdFavorite } from "react-icons/md";
+import { IoSearchSharp } from "react-icons/io5";
+import { VscLibrary } from "react-icons/vsc";
 import { Footer } from "../components/TemplateMain/Footer/Index";
 import styles from "../styles/Home.module.scss";
+import { ButtonBackground } from "../components/Buttons/ButtonBackground/Index";
+import { AiOutlinePlus } from "react-icons/ai";
 
 const Home: NextPage = () => {
   return (
@@ -17,14 +23,45 @@ const Home: NextPage = () => {
           </ul>
 
           <ul>
-            <li>inicio</li>
-            <li>buscar</li>
-            <li>sua biblioteca</li>
+            <li>
+              <RiHome2Line />
+              <h3>Início</h3>
+            </li>
+
+            <li>
+              <IoSearchSharp />
+              <h3>Buscar</h3>
+            </li>
+
+            <li>
+              <VscLibrary />
+              <h3>Sua Biblioteca</h3>
+            </li>
           </ul>
 
           <ul>
-            <li>criar playlist</li>
-            <li>músicas curtidas</li>
+            <li>
+              <ButtonBackground
+                style={{
+                  background: "#FFF",
+                  marginRight: "5px",
+                }}
+              >
+                <AiOutlinePlus />
+              </ButtonBackground>
+              <h3>Criar Playlist</h3>
+            </li>
+            <li>
+              <ButtonBackground
+                style={{
+                  background: "linear-gradient(135deg,#450af5,#c4efd9)",
+                  marginRight: "5px",
+                }}
+              >
+                <MdFavorite fill="#FFF" />
+              </ButtonBackground>
+              <h3>Músicas Curtidas</h3>
+            </li>
           </ul>
         </nav>
 
